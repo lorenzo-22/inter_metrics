f0abd2511f1f6c671f2c3fd12df1d30d9612a970
JosefineTM <jostvermei@gmail.com>
2025-12-03 15:18:25 +0100

    hased stuff that does not work as I workn on main upps

--- inter_tool_metrics.py
+++ inter_tool_metrics.py
@@ -64,10 +64,11 @@ def calculate_metrics(all_results, file_names, pairs, top_n = 50):
 def main():
     parser = argparse.ArgumentParser(description='Welch t-test benchmark runner')
 
-    parser.add_argument('--results', type=str,
-                        help='path to all results', required = True)
+    # parser.add_argument('--results', type=str,
+    #                     help='path to all results', required = True)
     parser.add_argument('--output_dir', type=str,
                         help='output directory to store results of metrics.')
+    
     
     try:
         args = parser.parse_args()
@@ -75,14 +76,14 @@ def main():
         parser.print_help()
         sys.exit(0)
 
-    results_dir = Path(args.results)
+    results_dir = Path(os.path.join(os.getcwd(), 'out'))
     output_dir = Path(args.output_dir)
 
     print(f"Input directory:  {results_dir}")
     print(f"Output directory: {output_dir}")
 
     # gather _results.csv files
-    all_files = sorted(results_dir.glob("*_results.csv"))
+    all_files = sorted(results_dir.rglob("*_results.csv"))
 
     if not all_files:
         print("ERROR: No '_results.csv' files found!")
@@ -90,24 +91,25 @@ def main():
 
     print(f"Found {len(all_files)} files:")
     for f in all_files:
-        print("  -", f.name)
+        print(f)
 
-    # save file names
-    file_names = [f.stem for f in all_files]
+    # # save file names
+    # file_names = [f.stem for f in all_files]
+    # print(file_names)
 
-    # Make unique pairs of the runs:
-    indices = list(range(len(all_files)))
-    pairs = list(combinations(indices, 2))
+    # # Make unique pairs of the runs:
+    # indices = list(range(len(all_files)))
+    # pairs = list(combinations(indices, 2))
 
-    # Load files and calculate FDR-adjusted p-value and rank
-    all_results = load_and_process_files(all_files)
+    # # Load files and calculate FDR-adjusted p-value and rank
+    # all_results = load_and_process_files(all_files)
     
-    # Calculate metrics
-    concordance_df = calculate_metrics(all_results, file_names=file_names, pairs=pairs)
+    # # Calculate metrics
+    # concordance_df = calculate_metrics(all_results, file_names=file_names, pairs=pairs)
 
-    print(concordance_df)
+    # print(concordance_df)
 
-    concordance_df.to_csv(os.path.join(args.output_dir, 'concordance_scores.csv'))
+    # concordance_df.to_csv(os.path.join(args.output_dir, 'concordance_scores.csv'))
 
 if __name__ == "__main__":
     main()
